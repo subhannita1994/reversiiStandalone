4b30f5b7ed1213fca04e331a6d61c54f1c4199cf
ankitjana <ankit92005@gmail.com>
2020-05-07 21:00:48 -0400

    websocket

---
+++ src/application/MessageContent.java
@@ -0,0 +1,18 @@
+package application;
+
+public class MessageContent {
+
+	private String content;
+
+	  public MessageContent() {
+	  }
+
+	  public MessageContent(String content) {
+	    this.content = content;
+	  }
+
+	  public String getContent() {
+	    return content;
+	  }
+
+}
