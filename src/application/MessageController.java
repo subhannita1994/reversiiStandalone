7773cdc3f9d5b5b29a2afd9ae8deefdc34d68bc4
ankitjana <ankit92005@gmail.com>
2020-05-07 21:27:35 -0400

    websocket

--- src/application/MessageController.java
+++ src/application/MessageController.java
@@ -1,5 +1,20 @@
 package application;
 
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.util.HtmlUtils;
+
+@Controller
 public class MessageController {
+
+
+  @MessageMapping("/hello")
+  @SendTo("/topic/content")
+  public MessageContent content(Message message) throws Exception {
+    Thread.sleep(1000); // simulated delay
+    return new MessageContent("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
+  }
 
 }
